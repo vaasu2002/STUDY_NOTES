60c1bb9aeb1351a8d1fe46e5bd5d005f6605c098
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2025-08-20 14:34:09 +0530

    Update virtual_proxy.cpp

--- System Design LLD/Structural Design Pattern/Proxy Design Pattern/virtual_proxy.cpp
+++ System Design LLD/Structural Design Pattern/Proxy Design Pattern/virtual_proxy.cpp
@@ -15,3 +15,19 @@ class BitImage:Image{
 		// drawing image
 	}
 };
+
+class LazyBitImage:Image{
+	MetaData m_metaData;
+	vector<vector<int>> image;
+	mutable  std::unique_ptr<BitImage> m_bitImage{nullptr};
+	public:
+	BitImage(MetaData metaData):m_metaData(metaData){}
+	void draw() override{
+		if(m_bitImage == nullptr){
+			// Constructor will be called will load the object
+			m_bitImage = std::make_unique<BitImage>(m_metaData);
+		}
+		// actual drawing of object
+		m_bitImage->draw();
+	}
+};
